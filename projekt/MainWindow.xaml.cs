de44ddbac1269dee6dc42db9049d4742970e2bca
minusLipton <mamutantum@gmail.com>
2025-01-28 17:44:23 +0100

    Beta 0.02

--- projekt/MainWindow.xaml.cs
+++ projekt/MainWindow.xaml.cs
@@ -1,30 +1,67 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace TimePlannerApp
 {
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Task> Tasks { get; set; } = new ObservableCollection<Task>();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void AddTaskButton_Click(object sender, RoutedEventArgs e)
         {
-            // Tutaj dodaj logikę dodawania zadania
-            MessageBox.Show("Dodaj zadanie kliknięte!");
+            // Example of adding a new task
+            Tasks.Add(new Task
+            {
+                TaskName = "Example Task",
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddHours(2).AddMinutes(30),
+                HoursSpent = 2,
+                MinutesSpent = 30
+            });
+
+            UpdateTotalTimeSpent();
         }
 
         private void DeleteTaskButton_Click(object sender, RoutedEventArgs e)
         {
-            // Tutaj dodaj logikę usuwania zadania
-            MessageBox.Show("Usuń zadanie kliknięte!");
+            // Example of deleting a task
+            var taskToRemove = (Task)((Button)sender).DataContext;
+            Tasks.Remove(taskToRemove);
+
+            UpdateTotalTimeSpent();
         }
 
         private void FilterButton_Click(object sender, RoutedEventArgs e)
         {
-            // Tutaj dodaj logikę filtrowania zadań
-            MessageBox.Show("Filtruj kliknięte!");
+            // Handle filtering tasks based on daily, weekly, or monthly
+            MessageBox.Show("Filtruj clicked!");
         }
+
+        private void UpdateTotalTimeSpent()
+        {
+            int totalMinutes = Tasks.Sum(t => t.HoursSpent * 60 + t.MinutesSpent);
+            int hours = totalMinutes / 60;
+            int minutes = totalMinutes % 60;
+            TotalTimeSpentTextBlock.Text = $"{hours} hours {minutes} minutes";
+        }
+    }
+
+    public class Task
+    {
+        public string TaskName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public int HoursSpent { get; set; }
+        public int MinutesSpent { get; set; }
     }
 }
